13874b9395d896dd79771080a49c7a855387fa3a
Mike Liu <m7liu@ucsd.edu>
2020-03-02 16:48:12 -0800

    comments

--- src/Arduino/Stepper_Motor/Stepper_Motor.ino
+++ src/Arduino/Stepper_Motor/Stepper_Motor.ino
@@ -1,11 +1,11 @@
 #include <Stepper.h>
 
-const float STEPS_PER_REV = 32;
-const float GEAR_RED = 64;
-const float STEPS_PER_OUT_REV = STEPS_PER_REV * GEAR_RED;
+const float STEPS_PER_REV = 32; //steps of the motor
+const float GEAR_RED = 64; //total steps to the gear
+const float STEPS_PER_OUT_REV = STEPS_PER_REV * GEAR_RED; //counts revolution
 
 int StepsRequired;
-Stepper steppermotor(STEPS_PER_REV, 8, 10, 9, 11);
+Stepper steppermotor(STEPS_PER_REV, 8, 10, 9, 11); //pins A08, A10, A09, A11
 
 void setup() {
   // put your setup code here, to run once:
