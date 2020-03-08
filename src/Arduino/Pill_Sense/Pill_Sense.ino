200b37c7156d7879039c43a918b4569b4bf4ae4f
Mon Morera <mmorera@ucsd.edu>
2020-03-07 20:28:18 -0800

    Fixed sensor

--- src/Arduino/Pill_Sense/Pill_Sense.ino
+++ src/Arduino/Pill_Sense/Pill_Sense.ino
@@ -1,5 +1,6 @@
 
 int run = 0;
+int power = 0;
 void setup()
 {
   Serial.begin(115200);
@@ -9,6 +10,11 @@ void setup()
 
 void loop()
 {
+  receiveMessage();
+  if(power>1){
+    run=0;
+    power=0;
+  }
   while(run==0){
     runStepper();
     if(runSense()==true){
